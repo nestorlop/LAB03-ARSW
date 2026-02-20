41db7f1f542b79330d49a3d331a81c0b56c3745d
Nestor Lopez <nestor.lopez-c@mail.escuelaing.edu.co>
2026-02-19 22:03:30 -0500

    fix: fixing  filters

--- src/main/java/edu/eci/arsw/blueprints/filters/IdentityFilter.java
+++ src/main/java/edu/eci/arsw/blueprints/filters/IdentityFilter.java
@@ -1,14 +1,20 @@
 package edu.eci.arsw.blueprints.filters;
 
 import edu.eci.arsw.blueprints.model.Blueprint;
+
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 /**
  * Default filter: returns the blueprint unchanged.
  * This matches the baseline behavior of the reference lab before students implement custom filters.
  */
+
 @Component
+@Profile("!redundancy & !undersampling")
 public class IdentityFilter implements BlueprintsFilter {
     @Override
-    public Blueprint apply(Blueprint bp) { return bp; }
+    public Blueprint apply(Blueprint bp) {
+        return bp;
+    }
 }
